46242ce8ae01fe039654306b135ecc1883ce227f
Mynamisboseong <wow456869@gmail.com>
2024-10-11 18:10:46 +0900

    carousels dev init

--- testreact/src/components/layouts/carousels.js
+++ testreact/src/components/layouts/carousels.js
@@ -2,13 +2,14 @@
 
     function UncontrolledExample() {
     return (
-        <Carousel>
-        <Carousel.Item>
+        <Carousel style={{width: 'auto', height: '800px', overflow: 'hidden', backgroundColor: '#abe0f5'}}>
+        <Carousel.Item style={{ height: '800px' }}>
             
             <img
                     className="d-block w-100"
                     src='/eco_slide_1.jpg'
                     alt="First slide"
+                    style={{ objectFit: 'contain', width: '100%', height: '100%' }}
             />
 
             <Carousel.Caption>
@@ -16,12 +17,13 @@
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+        <Carousel.Item style={{ height: '800px' }}>
             
             <img
                     className="d-block w-100"
                     src='/eco_slide_2.jpg'
                     alt="Second slide"
+                    style={{ objectFit: 'contain', width: '100%', height: '100%' }}
                 />
                 
             <Carousel.Caption>
@@ -29,12 +31,13 @@
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+        <Carousel.Item style={{ height: '800px' }}>
             
             <img
                     className="d-block w-100"
                     src='/eco_slide_3.jpg'
                     alt="Third slide"
+                    style={{ objectFit: 'contain', width: '100%', height: '100%' }}
                 />
             
             <Carousel.Caption>
